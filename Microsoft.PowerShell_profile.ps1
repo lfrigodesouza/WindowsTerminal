e5102cc7940ed4171a3cbc6950bc48d140fc4e36
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2020-07-28 21:05:05 -0300

    Novas configurações para terminal preview

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,23 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+
 Import-Module oh-my-posh
 [System.Environment]::SetEnvironmentVariable("SSH_AUTH_SOCK", $null)
 [System.Environment]::SetEnvironmentVariable("SSH_AGENT_PID", $null)
 Set-Theme Paradox
-# Start-SshAgent
-# Add-SshKey (Resolve-Path ~\.ssh\id_rsa.txt)
-# $GitPromptSettings.DefaultForegroundColor = "DarkCyan"
-# $GitPromptSettings.DefaultPromptSuffix = '`n$(''>'' * ($nestedPromptLevel + 1)) '
-# $GitPromptSettings.DefaultPromptPrefix = '$env:USERNAME@$(hostname) '
+
+function Update-Repositories() {
+    $basePath = 'C:\Repos\'
+    $reposPath = Get-ChildItem -Path C:\Repos | Where-Object {$_.Name -Match 'easynvest.*' }
+
+    ForEach($i in $reposPath){
+        $curPath = "$($basePath)$($i.Name)"
+            cd $curPath
+            Write-Host "processando $($curPath)" -ForegroundColor Green
+            git checkout develop
+            git fetch -p
+            git pull
+            git branch -v | wsl grep 'gone' | %{$_.Split(' ')[2];} | %{git branch -D $_}
+    }
+    cd $basePath
+}
