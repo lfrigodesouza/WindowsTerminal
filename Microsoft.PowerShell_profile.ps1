9ecc5b7dc42900b423e571e54ac1e269b8a98610
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2021-03-23 18:14:08 -0300

    Altera perfil do PowerShell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,28 @@
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
-
-Import-Module oh-my-posh
 [System.Environment]::SetEnvironmentVariable("SSH_AUTH_SOCK", $null)
 [System.Environment]::SetEnvironmentVariable("SSH_AGENT_PID", $null)
-Set-Theme Paradox
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme ~/.poshthemes/lsz.omp.json
+
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+        param($commandName, $wordToComplete, $cursorPosition)
+                dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+                               [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+                                       }
+}
+
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+}
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 function Update-Repositories() {
     $basePath = 'C:\Repos\'
-    $reposPath = Get-ChildItem -Path C:\Repos | Where-Object {$_.Name -Match 'easynvest.*' }
+    $reposPath = Get-ChildItem -Path C:\Repos | Where-Object {$_.Name -Match 'repo.*' }
 
     ForEach($i in $reposPath){
         $curPath = "$($basePath)$($i.Name)"
